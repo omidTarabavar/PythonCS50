03e85716566fd21c190c66782e8795259cde89fc
Omid Tarabavar <omidtarab1300@gmail.com>
2023-08-09 03:01:12 +0430

    Modify hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,24 @@
+"""
+Multi
+line
+comment
+"""
+
+# Ask user for their name
 name = input("What's your name? ")
+
+# Say hello to user
 print("hello,")
 print(name)
+
+print("hello, "+name)
+print("hello,",name) # it automatically insert space when doing this
+
+print("hello, ", end ="")
+print(name)
+
+print("hello",name,sep = "???")
+print('hello, "friend"')  # fix1 for corner case
+print("hello, \"friend\"") # fix2 for corner case
+
+print(f"hello, {name}")
